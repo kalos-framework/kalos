8152d871726650fa9a2e48317c2405e4d1796c14
Pete Houston <contact@petehouston.com>
2019-08-07 18:31:22 -0500

    Provide event for middleware stack.

--- src/middleware.js
+++ src/middleware.js
@@ -1,3 +1,5 @@
+import emitter from './event_emitter';
+
 const log = require('debug')('kalos:middleware');
 
 class Middleware {
@@ -8,20 +10,27 @@ class Middleware {
     use(middleware) {
         // stack order LIFO
         this.stack.push(middleware);
+        emitter.emit('Middleware:use', middleware);
     }
 
     dispatch(req, res, done) {
+        emitter.emit('Middleware:dispatch', req, res);
+
         const iterator = (index) => {
             if (index >= this.stack.length) {
                 log("finish middleware, calling router....");
+                emitter.emit('Middleware:dispatch:success', req, res);
                 return done(req, res);
             }
+
             try {
                 this.stack[index].call(this, req, res, () => {
+                    emitter.emit('Middleware:dispatch:next', index);
                     iterator.call(this, ++index);
                 });
             } catch (e) {
                 log("middleware error: %o", e);
+                emitter.emit('Middleware:dispatch:error', e);
                 return done(req, res, e);
             }
         };
