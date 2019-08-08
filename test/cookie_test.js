f020af6f16fab932cb6d48278a7856997052fc7e
Long Tran <longthb3112@gmail.com>
2019-08-08 11:37:57 -0500

    update test cookie

--- test/cookie_test.js
+++ test/cookie_test.js
@@ -1,9 +1,13 @@
 import Kalos from '../index';
 
-const server = new Kalos.Server();
+const server = new Kalos.Server({port:1222});
+const router = new Kalos.Router();
 
-server.configRouter(new Kalos.Router());
-
+router.get("/hello",(req,res) => {
+    console.log(res.headers.cookie);
+    res.end("cookie tested");
+});
+server.configRouter(router);
 server.start(() => {
     console.log('server started');
 });
